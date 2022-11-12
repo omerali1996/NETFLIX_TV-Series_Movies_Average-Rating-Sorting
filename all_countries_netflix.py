13f6e12b2e77d67d95964edc0dcb33aeefdc6430
Ömer Ali Uygun <112541841+omerali1996@users.noreply.github.com>
2022-11-12 15:53:44 +0300

    Update all_countries_netflix.py

--- all_countries_netflix.py
+++ all_countries_netflix.py
@@ -17,13 +17,13 @@ data = pd.read_excel("Kaggle_dosyalar/all-weeks-countries.xlsx")
 df=data.copy()
 
 def Analysis(df):
-    print(5*"*"+"DF1 Verisinin Analizi"+"*"*5)
-    print("\nGENEL BİLGİ")
+    print(5*"*"+"DF1 DATA ANALYSIS"+"*"*5)
+    print("\nGENERAL INFORMATION")
     print(f"\n{df.info()}")
     print(f"\n\nDESCRİBE \n{df.describe().T}")
-    print(f"\n\nSÜTUNLAR \n{[col for col in df.columns]}")
+    print(f"\n\nCOLUMNS \n{[col for col in df.columns]}")
     print(f"\n\nSHAPE\n{df.shape}")
-    print(f"\n\nBOŞ DEĞER\n{df.isnull().sum()}\n\n")
+    print(f"\n\nNULL VALUES\n{df.isnull().sum()}\n\n")
 
 Analysis(df)
 
@@ -46,34 +46,6 @@ def Arrange_Sort_df(df):
     return sort_df
 
 sort_df=Arrange_Sort_df(df)
-
-"""
-q1, q2, q3 = sort_df["analyze_week"].quantile([0.25, 0.5, 0.75])
-
-sort_df["week_rank"] = sort_df["analyze_week"]
-
-Q1 = sort_df[sort_df["analyze_week"] < q1]["cumulative_weeks_in_top_10_mean"].mean() * 20 / 100
-Q2 = sort_df[(sort_df["analyze_week"] > q1) & (sort_df["analyze_week"] <= q2)]["cumulative_weeks_in_top_10_mean"].mean() * 24 / 100
-Q3 = sort_df[(sort_df["analyze_week"] > q2) & (sort_df["analyze_week"] <= q3)]["cumulative_weeks_in_top_10_mean"].mean() * 26 / 100
-Q4 = sort_df[sort_df["analyze_week"] > q3]["cumulative_weeks_in_top_10_mean"].mean() * 30 / 100
-
-sort_df["week_rank"]=sort_df["week_rank"].apply(lambda x: Q1 if (x <= q1) else Q2 if (q2 >= x > q1) else Q3 if (q2 >= x > q1) else Q4)
-
-result_sorts = {"Q1": sort_df[sort_df["week"] < q1]["cumulative_weeks_in_top_10_mean"].mean() * 20 / 100,
-          "Q2": sort_df[(sort_df["week"] > q1) & (sort_df["week"] <= q2)][
-                    "cumulative_weeks_in_top_10_mean"].mean() * 24 / 100,
-          "Q3": sort_df[(sort_df["week"] > q2) & (sort_df["week"] <= q3)][
-                    "cumulative_weeks_in_top_10_mean"].mean() * 26 / 100,
-          "Q4": sort_df[sort_df["week"] > q3]["cumulative_weeks_in_top_10_mean"].mean() * 30 / 100
-          }
-sort_df=sort_df[["country_name","week_rank", "show_title"]]
-sort_df=sort_df.groupby(["country_name","show_title"]).agg({"week_rank":"mean"})
-sort_df.head()
-sort_df.reset_index(inplace=True)
-
-sort_df[sort_df["country_name"]=="Argentina"].sort_values(by="week_rank",ascending=False,ignore_index=True)
-
-"""
 
 ###Sorting by Average, Weekly
 
@@ -126,14 +98,14 @@ month=Montly_Sort(sort_df,"TV","Argentina")
 def Compare(df,category,country,thres=[0.2,0.24,0.26,0.3]):
     week=Weekly_Sort(df,category,country)
     month=Montly_Sort(df,category,country)
-    ifade=""
+    string=""
     if category == "TV":
-        ifade += "dizisi"
+        string += "TV-Series"
     else:
-        ifade += "filmi"
-    for x, col in enumerate(week["show_title"]):
-        print(f"\nHaftalık ortalamada {x}. olan {col} {ifade}=> "
-              f"Aylık ortalamada {(month[month['show_title'] == col]).index[0]}. olmuştur")
+        string += "Movie"
+    for x, col in enumerate(week["show_title"].head(20)):
+        print(f"\nAlthough  {col} is {string} {x}. Weekly_sort=> "
+              f"{(month[month['show_title'] == col]).index[0]}. on Monthly_sort")
 
 Compare(sort_df,"Films","Vietnam")
 
